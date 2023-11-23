b19f680a80aa0a734814df41213198b05c3dacc0
Somrat Hossain <somrat945hossain@gmail.com>
2023-11-23 13:47:39 +0600

    last touch

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,10 +14,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
    int i = -1;
-  ModelData? modelData;
+
   List<ModelData> modelDataList = [];
-  // var users = List<ModelData>();
- Future< void> getPostCommentData() async{
+  Future< void> getPostCommentData() async{
       var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts/1/comments"));
       var list = jsonDecode(response.body) as List;
       
@@ -26,17 +25,15 @@ class _HomeScreenState extends State<HomeScreen> {
       });
      
     
-      print(modelDataList);
+      debugPrint(modelDataList.toString());
        
   }
   @override
   void initState() {
-    //  modelDataList.map((e) => getPostCommentData()).toList();
-    // print("less $modelDataList");
      getPostCommentData();
-    // TODO: implement initState
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,34 +44,31 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           Expanded(
             flex: 3,
-            child: Container(
-              
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: modelDataList.length,
-                itemBuilder: (_, index){
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      trailing: IconButton(onPressed: (){
-                            //comment show function
-                            setState(() {
-                              i = index;
-                            });
-                          
-                      }, icon: Icon(Icons.comment, color: Colors.black,)),
-                      tileColor: Colors.purple.shade100,
-                      title: Text("Post : "+modelDataList[index].id.toString(), style: TextStyle(
-                    fontWeight: FontWeight.bold,fontSize: 20
-                  )),
-                      subtitle: Text(modelDataList[index].name.toString(),
-                      style: TextStyle(
-                       color: Colors.black
-                  )),
-                    ),
-                  );
-              }),
-            )),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: modelDataList.length,
+              itemBuilder: (_, index){
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    trailing: IconButton(onPressed: (){
+                          //comment show function
+                          setState(() {
+                            i = index;
+                          });
+                        
+                    }, icon: const Icon(Icons.comment, color: Colors.black,)),
+                    tileColor: Colors.purple.shade100,
+                    title: Text("Post : ${modelDataList[index].id}", style: const TextStyle(
+                  fontWeight: FontWeight.bold,fontSize: 20
+                )),
+                    subtitle: Text(modelDataList[index].name.toString(),
+                    style: const TextStyle(
+                     color: Colors.black
+                )),
+                  ),
+                );
+            })),
             Expanded(
             flex: 1,
             // ignore: unnecessary_null_comparison
@@ -84,18 +78,18 @@ class _HomeScreenState extends State<HomeScreen> {
                 tileColor: Colors.amber.shade200,
                 title: Padding(
                   padding: const EdgeInsets.all(4.0),
-                  child: Text("Post : " + modelDataList[i].id.toString(), style: TextStyle(
+                  child: Text("Post : ${modelDataList[i].id}", style: const TextStyle(
                     fontWeight: FontWeight.bold,fontSize: 20
                   ),),
                 ),
                 subtitle: Padding(
                   padding: const EdgeInsets.all(4.0),
-                  child: Text("COMMENT : " + modelDataList[i].body.toString(), style: TextStyle(
+                  child: Text("COMMENT : ${modelDataList[i].body}", style: const TextStyle(
                     fontSize: 12,color: Colors.black
                   ),),
                 ),
               ),
-            ): ListTile(title: Text('No comment'),))
+            ): const ListTile(title: Text('No comment'),))
         ],
       ),
     );
